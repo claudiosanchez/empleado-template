40e3889f3eff41bd0da787c957e6254fab8b45d1
Eliezer Pujols <eliezerpujols@outlook.com>
2016-01-26 16:52:03 -0400

    Created Home and Jobs List Sections

---
+++ js/empleado.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+   // Pre-loader
+   $("#pre-loader").fadeOut(1500);
+
+   // Setting Vegas
+   $("#intro").vegas({
+      slides: [
+         { src: "img/slide1.jpg" },
+         { src: "img/slide4.jpg" },
+         { src: "img/slide3.jpg" }
+      ]
+   });
+
+   // Setting Carousel
+   $("#owl-client").owlCarousel({
+      autoHeight: true,
+      autoPlay: true,
+      pagination: false,
+      navigation: true,
+      navigationText: [
+	      "<span class='arrow left'></span>",
+	      "<span class='arrow right'></span>"
+      ],
+      singleItem: true,
+      transitionStyle:"fade"
+   });
+});
